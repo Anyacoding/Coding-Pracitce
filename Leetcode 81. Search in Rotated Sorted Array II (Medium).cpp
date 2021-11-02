faaa24b7c786c1d1269975fa841c6904ab3f7cc3
Anyacoding <85947877+Anyacoding@users.noreply.github.com>
2021-11-02 23:37:37 +0800

    旋转数组的两种解法，嘤嘤嘤
    二分法看着简单，实现的细节却非常多，不可忽视

--- Leetcode 81. Search in Rotated Sorted Array II (Medium).cpp
+++ Leetcode 81. Search in Rotated Sorted Array II (Medium).cpp
@@ -1,3 +1,4 @@
+//法一：暴力搜索法时间复杂度O(N)，显然不是最优解
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
@@ -15,6 +16,48 @@ public:
                 return true;
             } else if (nums[right] != target) {
                 --right;
+            }
+        }
+        return false;
+    }
+};
+
+//方法二：二分查找法 时间复杂度O(logn)最优解
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        if (nums.size() == 0) {
+            return false;
+        }
+        int start = 0, end = nums.size() - 1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (nums[mid] == target) {
+                return true;
+            }
+            //左右区间至少有一个区间递增，遇到特殊情况判断不了，就首尾都改变一下位置
+            if (nums[mid] == nums[start] && nums[mid] == nums[end]) {
+                ++start;
+                --end;
+            } else if (nums[mid] >= nums[start]) {
+                //左区间递增
+                if (nums[start] <= target && target < nums[mid]) {
+                    //target可能在[start, mid - 1]区间中
+                    end = mid - 1;
+                } else {
+                    //target可能在[mid + 1, end]区间中
+                    start = mid + 1;
+                }
+
+            } else {
+                //右区间递增
+                if (nums[end] >= target && target > nums[mid]) {
+                    //target可能在[mid + 1, end]区间中
+                    start = mid + 1;
+                } else {
+                    //target可能在[start, mid - 1]区间中
+                    end = mid - 1;
+                }
             }
         }
         return false;
