2ee7d0aad2577809c698edb0f2f04222d0997289
Anyacoding <85947877+Anyacoding@users.noreply.github.com>
2021-12-03 10:42:18 +0800

    等差数列划分，一种是自己想的暴力法，另一种是题解的dp法

--- Leetcode 413. Arithmetic Slices (Medium).cpp
+++ Leetcode 413. Arithmetic Slices (Medium).cpp
@@ -1,3 +1,4 @@
+法一：自己想的暴力法
 class Solution {
 public:
     int numberOfArithmeticSlices(vector<int>& nums) {
@@ -26,3 +27,51 @@ public:
         return ans;
     }
 };
+
+法二：标准动态规划
+解题思路
+定义dp[i]表示加入第i个数字，对答案的贡献值。
+那么答案累加这些个贡献
+我们会发现
+如果
+1 2 3
+加入一个4
+那么有3种组合
+1 2 3
+2 3 4
+1 2 3 4
+新加入的nums[i]=4的贡献是3-1=2
+在加入一个5
+1 2 3
+2 3 4
+3 4 5
+1 2 3 4
+2 3 4 5
+1 2 3 4 5
+新加入的num[i]=5的贡献是6-3=3
+
+我们会发现如果一个数和前面的2个数成等差，那么他的贡献就是dp[i-1]+1
+当2 4 6的时候加8也是同理
+
+作者：jiao-chun-peng
+链接：https://leetcode-cn.com/problems/arithmetic-slices/solution/dong-tai-gui-hua-by-jiao-chun-peng-oine/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
+class Solution {
+public:
+    int numberOfArithmeticSlices(vector<int>& nums) {
+        int n = nums.size();
+        if (n < 3) return 0;
+        int ans = 0;
+        vector<int> dp(n, 0);
+        for (int i = 2; i < n; ++i) {
+            if (nums[i] - nums[i - 1] == nums[i - 1] - nums[i - 2]) {
+                dp[i] = dp[i - 1] + 1; //状态转移方程
+                ans += dp[i];
+            }
+        }
+        return ans;
+    }
+};
+
