00a013f350b8fc1f821ea6aeebf3dfdacd41ccc1
Anyacoding <85947877+Anyacoding@users.noreply.github.com>
2021-12-03 22:51:59 +0800

    最小路径和，用动态规划最重要的是找到状态转移方程

--- Leetcode 64. Minimum Path Sum (Medium).cpp
+++ Leetcode 64. Minimum Path Sum (Medium).cpp
@@ -20,3 +20,34 @@ public:
         return ans;
     }
 };
+
+法二：二维动态规划
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        vector<vector<int>>dp (grid.size(), vector<int>(grid[0].size(), 99999)); //对dp数组进行一个初始的化
+        for (int i = 0; i < grid.size(); ++i) {
+            for (int j = 0; j < grid[i].size(); ++j) {
+                //左上角做特殊处理
+                if (i == 0 && j == 0) {
+                    dp[i][j] = grid[i][j];
+                } 
+                //第一行除左上角外做特殊处理
+                else if (i == 0 && j != 0) {
+                    dp[i][j] = dp[i][j - 1] + grid[i][j];
+                }
+                //第一列除左上角外做特殊处理
+                else if (i != 0 && j == 0) {
+                    dp[i][j] = dp[i - 1][j] + grid[i][j];
+                }
+                //其余都可以套用状态转移方程
+                else {
+                    dp[i][j] = min(dp[i][j - 1], dp[i - 1][j]) + grid[i][j];
+                }
+            }
+        }
+        int m = grid.size() - 1;
+        int n = grid[0].size() - 1;
+        return dp[m][n];
+    }
+};
