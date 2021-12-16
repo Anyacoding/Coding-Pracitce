71f03efe47278a12534a80f377a089e1d58c575d
Anyacoding <85947877+Anyacoding@users.noreply.github.com>
2021-12-16 19:19:40 +0800

    一和零，01背包问题，分别采用了未压缩和压缩两种版本

--- Leetcode 474. Ones and Zeroes (Medium).cpp
+++ Leetcode 474. Ones and Zeroes (Medium).cpp
@@ -39,3 +39,39 @@ public:
         return dp[len][m][n];
     }
 };
+
+法二：滚动数组路径压缩
+class Solution {
+public:
+    pair<int, int> count(const string& str) {
+        int count0 = 0;
+        int count1 = 0;
+        for (const char it : str) {
+            if (it == '0') {
+                ++count0;
+            }
+            else {
+                ++count1;
+            }
+        }
+        return pair<int, int>(count0, count1);
+    }
+    int findMaxForm(vector<string>& strs, int m, int n) {
+        int len = strs.size();
+        //dp[i][j][k] 表示输入字符串在子区间 [0, i] 能够使用 j 个 0 和 k 个 1 的字符串的最大数量。
+        //这里使用滚动数组降低一个维度
+        int dp[m + 1][n + 1];
+        int count0, count1;
+        //滚动数组可以全部初始化为0
+        memset(dp, 0, sizeof(dp));
+        for (int i = 1; i <= len; ++i) {
+            auto[count0, count1] = count(strs[i - 1]);
+            for (int j = m; j >= count0; --j) {
+                for (int k = n; k >= count1; --k) {
+                        dp[j][k] = max(dp[j][k], dp[j - count0][k - count1] + 1);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+};
