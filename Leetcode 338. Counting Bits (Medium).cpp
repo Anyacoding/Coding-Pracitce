1186d143b0d0e894f9a9ff19aa78b089f10dd866
Anyacoding <85947877+Anyacoding@users.noreply.github.com>
2022-02-11 13:51:01 +0800

    比特位计数，dp优化加位运算

--- Leetcode 338. Counting Bits (Medium).cpp
+++ Leetcode 338. Counting Bits (Medium).cpp
@@ -18,3 +18,15 @@ public:
         return ans;
     }
 };
+
+法二：dp优化版本
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> dp(n + 1, 0);
+        for (int i = 1; i <= n; ++i) {
+            dp[i] = 1 & i? dp[i - 1] + 1 : dp[i >> 1];
+        }
+        return dp;
+    }
+};
