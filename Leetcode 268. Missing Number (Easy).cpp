635125b44d78a0d83287ccf177023b554678a03e
Anyacoding <85947877+Anyacoding@users.noreply.github.com>
2022-02-12 19:06:32 +0800

    丢失的数字

--- Leetcode 268. Missing Number (Easy).cpp
+++ Leetcode 268. Missing Number (Easy).cpp
@@ -9,3 +9,17 @@ public:
         return gauss - sum;
     }
 };
+法二：异或求消失的数，利用 x ∧ x = 0 和 x ∧ 0 = x 的特点，将数组内所有的数字进行按位异或。出现两次
+的所有数字按位异或的结果是 0，0 与出现一次的数字异或可以得到这个数字本身。我们手动使得nums中出现的元素出现两次，那么消失的元素就只出现一次
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int ans = 0;
+        int n = nums.size();
+        for (int i = 0; i < n; ++i) {
+            ans ^= i + 1;
+            ans ^= nums[i];
+        }
+        return ans;
+    }
+};
