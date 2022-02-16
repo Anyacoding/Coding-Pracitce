71a25445a36d0ffdc81ec0a49a796a5406f80459
Anyacoding <85947877+Anyacoding@users.noreply.github.com>
2022-02-16 12:51:00 +0800

    找到所有数组中消失的数字

--- Leetcode 448. Find All Numbers Disappeared in an Array (Easy).cpp
+++ Leetcode 448. Find All Numbers Disappeared in an Array (Easy).cpp
@@ -13,3 +13,23 @@ public:
         return ans;
     }
 };
+
+法二:原地操作，常数空间
+对于第 i 个数字 nums[i]，我们位置 (nums[i] - 1) % n 的位置增加 n，这样不会覆盖原数组，因为 (nums[i] - 1) % n = (nums[i] - 1 + n) % n，
+这样如果最后遍历完数组，nums[i] 小于等于 n，就是数组中中消失的数字。
+
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        vector<int> ans;
+        int n = nums.size();
+        for (int i = 0; i < n; ++i) {
+            int index = (nums[i] - 1) % n;
+            nums[index] += n;
+        }
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] <= n) ans.push_back(i + 1);
+        }
+        return ans;
+    }
+};
