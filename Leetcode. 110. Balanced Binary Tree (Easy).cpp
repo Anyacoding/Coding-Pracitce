a685822dcad15cc2c32a93d2c8df84a2c7bcdf10
Anyacoding <85947877+Anyacoding@users.noreply.github.com>
2022-05-19 13:09:43 +0800

    平衡二叉树的检验

---
+++ Leetcode. 110. Balanced Binary Tree (Easy).cpp
@@ -0,0 +1,24 @@
+法一：检验是否是平衡二叉树的通用做法
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isBalanced(TreeNode* root) {
+        return helper(root) != -1;
+    }
+    int helper(TreeNode* root) {
+        if (root == nullptr) return 0;
+        int left = helper(root->left); int right = helper(root->right);
+        if (left == -1 || right == -1 || abs(left - right) > 1) return -1;
+        return 1 + max(left, right);
+    }
+};
