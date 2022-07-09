d61834fb3097f30432efea976b4c7748627d0da1
Anyacoding <85947877+Anyacoding@users.noreply.github.com>
2022-07-09 23:22:15 +0800

    404. 左叶子之和

---
+++ Leetcode 404. Sum of Left Leaves (Easy).cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int sum = 0;
+    int sumOfLeftLeaves(TreeNode* root) {
+        help(root);
+        return sum;
+    }
+
+    void help(TreeNode* root) {
+        if (root == nullptr) return;
+        sum += root->left && root->left->left == nullptr && root->left->right == nullptr ? root->left->val : 0;
+        help(root->left);
+        help(root->right);
+    }
+};
